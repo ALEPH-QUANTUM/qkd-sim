d610beb515508aec3e70743ce248b60aa99c0266
Rene Grempel <grempelrene@gmail.com>
2026-02-02 11:22:16 +0300

    Add simple channel noise model
    Added a function to simulate noise in a communication channel.

--- qkd/channel.py
+++ qkd/channel.py
@@ -1,1 +1,18 @@
+import random
+
+
+def noisy_channel(bits, noise_rate=0.02):
+    """
+    Flip bits with probability = noise_rate.
+    Models benign channel noise.
+    """
+    noisy_bits = []
+
+    for bit in bits:
+        if random.random() < noise_rate:
+            noisy_bits.append(1 - bit)
+        else:
+            noisy_bits.append(bit)
+
+    return noisy_bits
 
